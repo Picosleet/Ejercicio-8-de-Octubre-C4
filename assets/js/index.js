93c98a5d14ab686d2140f205e536d8d7da6ee8a3
javier Perez <cjperez74@hotmail.com>
2021-10-11 23:24:44 -0500

    second commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,11 +6,26 @@ document.getElementById("saveDataBtn").addEventListener("click",function(e){
     var note1=document.getElementById("note1").value
     var note2=document.getElementById("note2").value
     var note3=document.getElementById("note3").value
-    var promedio=document.getElementById("prom").value
-    console.log(studentID,studentName,note1,note2,note3,promedio)
+    
+    console.log(studentID,studentName,note1,note2,note3,myFunction())
     if(studentID=="" || studentName=="" || note1=="" ||note2=="" || note3=="")
         {alert("Debe llenar los campos!")}
-    else{localStorage.setItem(studentID,[studentName,note1,note2,note3,promedio])
+    else{localStorage.setItem(studentID,[studentName,note1,note2,note3,myFunction()])
         alert("Todo quedó guardado!")}
         
 })
+function myFunction() {  
+    var x,y,z,prom,text;  
+    x = document.getElementById("note1").value;  
+    y = document.getElementById("note2").value;  
+    z = document.getElementById("note3").value;  
+    
+    if (isNaN(x) || isNaN(y) || isNaN(z)) {  
+      text = "Es necesarios introducir dos números válidos";  
+    } else {  
+      //si no ponemos parseFloat concatenaría x con y  
+      prom = (parseFloat(x)+parseFloat(y)+parseFloat(z))/3; 
+
+      } 
+      return prom  
+}
